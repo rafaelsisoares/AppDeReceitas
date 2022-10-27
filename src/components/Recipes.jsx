1b348bdc5cb317e6c68b1441e419c006204e1356
Marcelo Vieira <marcelodefy15@gmail.com>
2022-10-27 16:32:28 -0300

    main-group-31-req 23

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 function Recipes({ recipes }) {
   const renderRecipesLimit = 12;
   const currRecipeCategory = useLocation().pathname.slice(1);
+  const currRecipeIdKey = currRecipeCategory === 'meals' ? 'idMeal' : 'idDrink';
 
   if (recipes.length > 0) {
     return (
@@ -12,25 +13,29 @@ function Recipes({ recipes }) {
         { recipes.length > 0 && recipes.map((recipe, index) => {
           if (index < renderRecipesLimit) {
             return (
-              <div
+              <Link
+                to={ `/${currRecipeCategory}/${recipe[currRecipeIdKey]}` }
                 key={ currRecipeCategory === 'meals' ? recipe.idMeal : recipe.idDrink }
-                className="recipe-card"
-                data-testid={ `${index}-recipe-card` }
               >
-                <img
-                  className="recipe-card-img"
-                  data-testid={ `${index}-card-img` }
-                  src={ currRecipeCategory
-                     === 'meals' ? recipe.strMealThumb : recipe.strDrinkThumb }
-                  alt=""
-                />
-                <h3
-                  className="recipe-title"
-                  data-testid={ `${index}-card-name` }
+                <div
+                  className="recipe-card"
+                  data-testid={ `${index}-recipe-card` }
                 >
-                  { currRecipeCategory === 'meals' ? recipe.strMeal : recipe.strDrink }
-                </h3>
-              </div>
+                  <img
+                    className="recipe-card-img"
+                    data-testid={ `${index}-card-img` }
+                    src={ currRecipeCategory
+                       === 'meals' ? recipe.strMealThumb : recipe.strDrinkThumb }
+                    alt=""
+                  />
+                  <h3
+                    className="recipe-title"
+                    data-testid={ `${index}-card-name` }
+                  >
+                    { currRecipeCategory === 'meals' ? recipe.strMeal : recipe.strDrink }
+                  </h3>
+                </div>
+              </Link>
             );
           }
           return null;
